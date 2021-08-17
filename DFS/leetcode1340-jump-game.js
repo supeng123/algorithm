4401d39b09061f4865c9bd64ffc07e503b0f8b2a
supeng123 <492526873@qq.com>
2021-08-17 22:13:23 +0800

    dfs

---
+++ DFS/leetcode1340-jump-game.js
@@ -0,0 +1,24 @@
+var jumpGame = function(arr, d) {
+    let len = arr.length
+    const memo = new Array(len).fill(-1)
+    let result = 1
+    arr.forEach((item, idx) => {
+        result = Math.max(result, dfs(idx))
+    })
+    return result
+
+    function dfs(index) {
+        if (memo[index] !== -1) return memo[index]
+        let res = 1
+        for (let i = index+1; i < len && i <= index + d && arr[index] > arr[i]; i++) {
+            res = Math.max(res, dfs(i)+1)
+        }
+
+        for (let i = index-1; i >= 0 && i >= index - d && arr[index] > arr[i]; i--) {
+            res = Math.max(res, dfs(i)+1)
+        }
+
+        memo[index] = res
+        return res
+    }
+}
