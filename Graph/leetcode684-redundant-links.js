8507d135629e903b86f4ad3e938e0f8eb3979cd4
supeng123 <492526873@qq.com>
2021-10-06 22:22:55 +0800

    graph

--- Graph/leetcode684-redundant-links.js
+++ Graph/leetcode684-redundant-links.js
@@ -1,3 +1,22 @@
-var redundantLinks = function() {
+var redundantLinks = function(edges) {
     //并查集
+    const father = new Array(edges.length+1).fill(0).map((_, index) => index)
+    const find = (x) => {
+        if (x === father[x]) {
+            return father[x]
+        }
+        father[x] = find(father[x]);
+        return father[x]
+    }
+    let res = []
+    for (let edge of edges) {
+        const fx = find(edge[0])
+        const fy = find(edge[1])
+        if (fx === fy) {
+            res = edge
+        } else {
+            father[fy] = fx
+        }
+    }
+    return res
 }
