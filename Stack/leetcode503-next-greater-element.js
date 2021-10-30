413456e15416bf142edd7067d32a085d5f557498
supeng123 <492526873@qq.com>
2021-10-30 22:29:39 +0800

    stack

--- Stack/leetcode503-next-greater-element.js
+++ Stack/leetcode503-next-greater-element.js
@@ -11,3 +11,17 @@ var nextGreaterElement = function(nums) {
     }
     return res
 }
+
+var nextGreaterElement = function(nums) {
+    const n = nums.length
+    const stack = []
+    const res = new Array(n)
+    for (let i = 2*n-1; i >= 0; i--) {
+        if (stack.length > 0 && stack[stack.length-1] <= nums[i%n]) {
+            stack.pop()
+        }
+        res[i%n] = stack.length > 0 ? stack[stack.lenght-1] : -1
+        stack.push(nums[i%n])
+    }
+    return res
+}
